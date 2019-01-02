68fd20fbfa8a0117de2bbde3b8c9138c7da6dabf
anthonyvno <anthony.vannoppen.y2475@student.hogent.be>
2019-01-02 17:37:53 +0100

    test update
    update test met commentaar

--- AndroidProject/app/src/androidTest/java/com/example/anthonyvannoppen/androidproject/UItest.kt
+++ AndroidProject/app/src/androidTest/java/com/example/anthonyvannoppen/androidproject/UItest.kt
@@ -21,30 +21,28 @@ class UItest {
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
+    // deze test kijkt wat er in het 1e element van de recyclerview zit: waarden moeten daaraan gelijk zijn
     @Test
-    fun memeList_onSelectMeme_showsCorrectMemeInfo(){
+    fun memeList_onSelectMeme_showsCorrectMemeInfo() {
 
-        //onView(withId(R.id.action_home)).perform(click())
         onView(withId(R.id.action_sort)).perform(click())
         onData(anything()).atPosition(0).perform(click())
         onView(withId(R.id.fragment_meme_list))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
-        onView(withId(R.id.text_detail_titel)).check(matches(withText("Mfw exams")))
-        onView(withId(R.id.text_detail_beschrijving)).check(matches(withText("Dit is een test")))
+        onView(withId(R.id.text_detail_titel)).check(matches(withText("AA")))
+        onView(withId(R.id.text_detail_beschrijving)).check(matches(withText("AA")))
 
     }
 
+    //deze test kijk of het 1e element van de recyclerview gesorteerd op games een meme categorie games zit
     @Test
-    fun select_correct_category(){
-
-        //onView(withId(R.id.action_sort)).perform(click())
-        //onData(anything()).atPosition(0).perform(click())
+    fun select_correct_category() {
 
         onView(withId(R.id.action_sort)).perform(click())
         onData(anything()).atPosition(3).perform(click())
         onView(withId(R.id.fragment_meme_list))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
 
         onView(withId(R.id.text_detail_categorie)).check(matches(withText("- Games")))
@@ -52,44 +50,40 @@ class UItest {
     }
 
 
+    //maakt een nieuwe meme en kijkt of deze in de recyclerview zit
     @Test
-     fun createNewMeme(){
-         //maak nieuwe meme
-         onView(withId(R.id.action_add)).perform(click())
-         onView(withId(R.id.text_add_titel)).perform(typeText("test"))
-         android.support.test.espresso.Espresso.closeSoftKeyboard()
-         onView(withId(R.id.text_add_op)).perform(typeText("test"))
-         android.support.test.espresso.Espresso.closeSoftKeyboard()
-         onView(withId(R.id.text_add_beschrijving)).perform(typeText("test"))
-         android.support.test.espresso.Espresso.closeSoftKeyboard()
-         onView(withId(R.id.text_add_afbeelding)).perform(typeText("test"))
-         android.support.test.espresso.Espresso.closeSoftKeyboard()
-         onView(withId(R.id.button_add_submit)).perform(click())
-         //check of deze in de recyclerview zit
-         onView(withId(R.id.fragment_meme_list)).check(matches(hasDescendant(withText("test"))))
+    fun createNewMeme() {
+        onView(withId(R.id.action_add)).perform(click())
+        onView(withId(R.id.text_add_titel)).perform(typeText("AA"))
+        android.support.test.espresso.Espresso.closeSoftKeyboard()
+        onView(withId(R.id.text_add_op)).perform(typeText("AA"))
+        android.support.test.espresso.Espresso.closeSoftKeyboard()
+        onView(withId(R.id.text_add_beschrijving)).perform(typeText("AA"))
+        android.support.test.espresso.Espresso.closeSoftKeyboard()
+        onView(withId(R.id.text_add_afbeelding)).perform(typeText("AA"))
+        android.support.test.espresso.Espresso.closeSoftKeyboard()
+
+        onView(withId(R.id.button_add_submit)).perform(click())
+        Thread.sleep(2000)
+        onView(withId(R.id.fragment_meme_list)).check(matches(hasDescendant(withText("AA"))))
 
 
+    }
 
-     }
-
+    //maakt een nieuwe comment en kijkt of deze in de recyclerview zit
     @Test
-    fun postNewComment(){
-        //onView(withId(R.id.action_home)).perform(click())
+    fun postNewComment() {
         onView(withId(R.id.action_sort)).perform(click())
         onData(anything()).atPosition(0).perform(click())
         onView(withId(R.id.fragment_meme_list))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(1,click()))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(3, click()))
         onView(withId(R.id.tekst_detail_commentOp)).perform(typeText("pim"))
         android.support.test.espresso.Espresso.closeSoftKeyboard()
         onView(withId(R.id.tekst_detail_commentTekst)).perform(typeText("pim"))
         android.support.test.espresso.Espresso.closeSoftKeyboard()
         onView(withId(R.id.button_detail_commentSubmit)).perform(click())
-        android.support.test.espresso.Espresso.pressBack()
-        onView(withId(R.id.fragment_meme_list))
-            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+        Thread.sleep(2000)
         onView(withId(R.id.comment_list)).check(matches(hasDescendant(withText("pim"))))
-
-
 
 
     }
